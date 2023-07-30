b0297bec8c3f5cfc2fa72975f703117ae4c58452
Anastasia <136612325+anastasiabohomolova@users.noreply.github.com>
2023-07-30 18:01:21 +0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,47 @@
+"""firstproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+class Page:
+    def test_func(request, test):
+        return HttpResponse(f'{test}')
+    def test_main_func(request, main):
+        return HttpResponse(f'test/{main}')
+    def main_page_python_func(request, python):
+        return HttpResponse(f'main_page/home/{python}')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<str:test>/', Page.test_func),
+    path('test/<str:main>/', Page.test_main_func),
+    path('main_page/home/<str:python>/', Page.main_page_python_func),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
